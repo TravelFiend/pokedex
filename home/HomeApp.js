50f115d77b5c8a26751f59ed9655384c3923dd9c
TravelFiend <mikegrace274@gmail.com>
2019-11-01 13:49:41 -0700

    home page header styling has returned

--- home/HomeApp.js
+++ home/HomeApp.js
@@ -1,12 +1,10 @@
 import Component from '../Component.js';
 import Header from '../common/Header.js';
 
-const hotBod = document.querySelector('body');
-
 class HomeApp extends Component {
-    onRender() {
+    onRender(el) {
         const header = new Header();
-        hotBod.prepend(header.renderDOM());
+        el.prepend(header.renderDOM());
     }
 
     renderHTML() {
